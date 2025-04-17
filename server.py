7b4b0aaf4973331de6098e3ff0061d847af5fdc0
Lumber4ack <protsyukvitalik@gmail.com>
2025-04-17 22:45:59 +0200

    added correct server implementation

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import socketserver
 import os
 import json
 from datetime import datetime
+import sqlite3
 
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
@@ -47,6 +48,13 @@ def decrypt_message(private_key, ciphertext):
     )
     return plaintext
 
+def send_message(sock, command, payload):
+    sock.sendall(json.dumps({
+        "timestamp": datetime.now().isoformat(),
+        "command": command,
+        "payload": payload
+    }).encode())
+
 class MyTCPHandler(socketserver.BaseRequestHandler):
     def handle(self):
         print(f"Connection established with {self.client_address[0]}:{self.client_address[1]}")
@@ -66,97 +74,179 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
             self.data = self.request.recv(4096)
             self.data = json.loads(self.data.decode())
             print(f"Received message: {self.data}")
-            if self.data["command"] == "register":
-                self.data["payload"]["username"] = decrypt_message(private_key, bytes.fromhex(self.data["payload"]["username"]))
-                if any(d.get("username") == self.data["payload"]["username"] for d in host_db):
-                    self.request.sendall(json.dumps({
-                        "timestamp": datetime.now().isoformat(),
-                        "command": "register",
-                        "payload": {
+            match self.data["command"]:
+                case "register":
+                    con = sqlite3.connect('database.db')
+                    cursor = con.cursor()
+                    cursor.execute("SELECT name FROM users")
+                    hosts = [host[0] for host in cursor.fetchall()]
+
+                    self.data["payload"]["username"] = decrypt_message(private_key, bytes.fromhex(self.data["payload"]["username"])).decode()
+                    #host = hosts.find_one({"username":self.data["payload"]["username"]})
+                    print(hosts)
+                    if self.data["payload"]["username"] in hosts:
+                        send_message(self.request, "register", {
                             "status": "fail",
                             "message": "Username already exists"
-                        }
-                    }).encode())
-                    break
-                else:
-                    host_db.append({"username":self.data["payload"]["username"], "public_key": client_public})
-                    self.request.sendall(json.dumps({
-                        "timestamp": datetime.now().isoformat(),
-                        "command": "register",
-                        "payload": {
+                        })
+                        con.close()
+                        break
+                    else:
+                        #hosts.insert_one({"username":self.data["payload"]["username"], "public_key": client_public.public_bytes(encoding=serialization.Encoding.PEM, format=serialization.PublicFormat.SubjectPublicKeyInfo)})
+                        cursor.execute("INSERT INTO users (name, pub) VALUES (?, ?)", (self.data["payload"]["username"], client_public.public_bytes(encoding=serialization.Encoding.PEM, format=serialization.PublicFormat.SubjectPublicKeyInfo)))
+                        
+                        send_message(self.request, "register", {
                             "status": "success",
                             "message": "User registered successfully"
-                        }
-                    }).encode())
-                    print(f"User {self.data['payload']['username']} registered successfully")
-            elif self.data["command"] == "login":
-                self.data["payload"]["username"] = decrypt_message(private_key, bytes.fromhex(self.data["payload"]["username"]))
-                if not any(d.get("username") == self.data["payload"]["username"] for d in host_db):
-                    self.request.sendall(json.dumps({
-                        "timestamp": datetime.now().isoformat(),
-                        "command": "login",
-                        "payload": {
+                        })
+                        print(f"User {self.data['payload']['username']} registered successfully")
+                    con.commit()
+                    con.close()
+                case "login":
+                    con = sqlite3.connect('database.db')
+                    cursor = con.cursor()
+                    cursor.execute("SELECT name FROM users")
+                    hosts = [host[0] for host in cursor.fetchall()]
+
+                    self.data["payload"]["username"] = decrypt_message(private_key, bytes.fromhex(self.data["payload"]["username"])).decode()
+                    #host = hosts.find_one({"username":self.data["payload"]["username"]})
+                    print(hosts)
+                    print(self.data["payload"]["username"])
+                    if self.data["payload"]["username"] not in hosts:
+                        send_message(self.request, "login", {
                             "status": "fail",
                             "message": "Username does not exist"
-                        }
-                    }).encode())
-                else:
-                    if self.data["payload"]["username"] in connected:
-                        self.request.sendall(json.dumps({
-                            "timestamp": datetime.now().isoformat(),
-                            "command": "login",
-                            "payload": {
+                        })
+                        con.close()
+                        break
+                    else:
+                        if self.data["payload"]["username"] in connected:
+                            send_message(self.request, "login", {       
                                 "status": "fail",
                                 "message": "User already logged in"
-                            }
-                        }).encode())
-                        break
-                    elif any(d.get("public_key") == client_public for d in host_db):
+                            })
+                            con.close()
+                            break
                         # Generate a random token for the user
-                        token = random.randint(100000, 999999)
+                        token = int(datetime.now().timestamp()) + random.randint(0, 1000000)
                         connected[self.data["payload"]["username"]] = [self.request, token]
-                        self.request.sendall(json.dumps({
-                            "timestamp": datetime.now().isoformat(),
-                            "command": "login",
-                            "payload": {
-                                "status": "success",
-                                "message": "User logged in successfully",
-                                "token": encrypt_message(client_public, str(token).encode()).hex(),
-                            }
-                        }).encode())
+                        send_message(self.request, "login", {
+                            "status": "success",
+                            "message": "User logged in successfully",
+                            "token-sig": encrypt_message(client_public, str(token).encode()).hex(),
+                        })
+                        con.close()
                         print(f"User {self.data['payload']['username']} logged in with token {token}")
-            elif self.data["command"] == "logout":
-                self.data["payload"]["username"] = decrypt_message(private_key, bytes.fromhex(self.data["payload"]["username"]))
-                if self.data["payload"]["username"] not in connected:
-                    self.request.sendall(json.dumps({
-                        "timestamp": datetime.now().isoformat(),
-                        "command": "logout",
-                        "payload": {
+                case "login-verif":
+                    self.data["payload"]["username"] = decrypt_message(private_key, bytes.fromhex(self.data["payload"]["username"])).decode()
+                    if self.data["payload"]["username"] not in connected:
+                        send_message(self.request, "login-verif", {
                             "status": "fail",
                             "message": "User not logged in"
-                        }
-                    }).encode())
-                    return
-                else:
-                    del connected[self.data["payload"]["username"]]
-                    self.request.sendall(json.dumps({
-                        "timestamp": datetime.now().isoformat(),
-                        "command": "logout",
-                        "payload": {
+                        })
+                        break
+                    else:
+                        token = decrypt_message(private_key, bytes.fromhex(self.data["payload"]["token-sig"]))
+                        if token == str(connected[self.data["payload"]["username"]][1]).encode():
+                            send_message(self.request, "login-verif", {
+                                "status": "success",
+                                "message": "User verified successfully"
+                            })
+                            print(f"User {self.data['payload']['username']} verified successfully")
+                        else:
+                            send_message(self.request, "login-verif", {
+                                "status": "fail",
+                                "message": "Token verification failed"
+                            })
+                            break
+                case "online-list":
+                    send_message(self.request, "online-list", {
+                        "status": "success",
+                        "users": list(connected.keys())
+                    })
+                    print(f"Online users: {list(connected.keys())}")
+                case "message":
+                    if self.data["payload"]["from"] not in connected:
+                        send_message(self.request, "message", {
+                            "status": "fail",
+                            "message": "User not logged in"
+                        })
+                        break
+                    """
+                    messages.insert_one({
+                            "from": self.data["payload"]["from"],
+                            "to": self.data["payload"]["to"],
+                            "aes": self.data["payload"]["aes"],
+                            "msg-cont": self.data["payload"]["msg-cont"],
+                            "timestamp": datetime.now().isoformat(timespec="seconds"),
+                    })
+                    """
+                    con = sqlite3.connect('database.db')
+                    cursor = con.cursor()
+                    cursor.execute("SELECT name FROM users")
+                    hosts = [host[0] for host in cursor.fetchall()]
+
+                    for receipient in self.data["payload"]["to"]:
+                        if receipient not in hosts:
+                            send_message(self.request, "message", {
+                                "status": "fail",
+                                "message": f"Recipient {receipient} does not exist"
+                            })
+                            con.close()
+                            break
+                        if receipient in connected:
+                            send_message(connected[receipient][0], "message", {
+                                "from": self.data["payload"]["from"],
+                                "to": receipient,
+                                "aes": self.data["payload"]["aes"],
+                                "msg-cont": self.data["payload"]["msg-cont"],
+                            })
+                            print(f"Retransmited message from {self.data['payload']['from']} to {self.data['payload']['to']}")
+
+                    cursor.execute("""INSERT INTO messages VALUES (NULL, ?, ?, ?)""", (
+                        self.data["sender_timestamp"],
+                        str(self.data["payload"]["to"]),
+                        str(self.data["payload"])
+                    ))
+                    con.commit()
+                    con.close()
+                case "sync":
+                    key = next((key for key, value in connected.items() if value[0] == self.request), None)
+
+                    if key is None:
+                        send_message(self.request, "sync", {
+                            "status": "fail",
+                            "message": "User not registered"
+                        })
+                        break
+                    
+                    con = sqlite3.connect('database.db')
+                    cursor = con.cursor()
+                    cursor.execute("SELECT payload FROM messages WHERE sender_timestamp BETWEEN ? AND ? AND receivers LIKE ?", (self.data["payload"]["from"], self.data["payload"]["to"], f"%{key}%"))
+                    msgs = [msgs[0] for msgs in cursor.fetchall()]
+                    
+                    send_message(self.request, "sync", {
+                        "status": "success",
+                        "payload": msgs
+                    })
+                    con.close()
+
+                case "logout":
+                    key = next((key for key, value in connected.items() if value[0] == self.request), None)
+                    if key is None:
+                        send_message(self.request, "logout", {
+                            "status": "fail",
+                            "message": "User not logged in"
+                        })
+                        return
+                    else:
+                        del connected[key]
+                        send_message(self.request, "logout", {
                             "status": "success",
                             "message": "User logged out successfully"
-                        }
-                    }).encode())
-                    print(f"User {self.data['payload']['username']} logged out successfully")
-                    return
-            #host_db.remove({"username":self.data["payload"]["username"], "public_key": client_public})
-        if self.data["payload"]["username"] in connected:
-            del connected[self.data["payload"]["username"]]
-
-        
-
-
-
+                        })
+                        print(f"User {key} logged out successfully")
+                        return
 
 class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     allow_reuse_address = True
@@ -194,15 +284,34 @@ def generate_keys(key_size):
         )
     return private_key, public_key
 
+def create_tables():
+    con = sqlite3.connect('database.db')
+    cursor = con.cursor()
+    table = """ CREATE TABLE IF NOT EXISTS users (
+                name VARCHAR(255) PRIMARY KEY,
+                pub VARCHAR(255) NOT NULL
+    ); """
+    cursor.execute(table)
+    table = """ CREATE TABLE IF NOT EXISTS messages (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                sender_timestamp VARCHAR(255) NOT NULL,
+                receivers VARCHAR(255) NOT NULL,
+                payload VARCHAR(255) NOT NULL
+    ); """
+    cursor.execute(table)
+    con.commit()
+    con.close()
+
 if __name__ == "__main__":
     HOST, PORT = "192.168.0.4", 9999
+    HOST = input("Enter the server address: ")
+    PORT = int(input("Enter the server port: "))
     KEY_SIZE = 2048
-
+    
+    create_tables()
     connected = {}
-    host_db = [{"username": "admin", "public_key": "admin"}]
 
     private_key, public_key = generate_keys(KEY_SIZE)
     with ThreadedTCPServer((HOST, PORT), MyTCPHandler) as server:
         print(f"Serving on {HOST}:{PORT}")
         server.serve_forever()
-
